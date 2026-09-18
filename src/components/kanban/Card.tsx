75aa33ab9f64903ba426d2c92ee68eb10a2d1a95
ardala <ardalanmahmoodi6@gmail.com>
2026-09-18 21:39:10 +0330

    add keyboard accessibilty

--- src/components/kanban/Card.tsx
+++ src/components/kanban/Card.tsx
@@ -1,5 +1,6 @@
+"use client"
 import { CardType } from "@/lib/types/kanban.type";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import DeleteCardButton from "./DeleteCardButton";
 import { useBoardStore } from "@/stores/board.store";
 import toast from "react-hot-toast";
@@ -17,10 +18,7 @@ type CardTypeProps = {
     cardId: string,
     sourceListId: number,
   ) => void;
-  onDragOver: (
-    e: React.DragEvent<HTMLDivElement>,
-    targetIndex: number,
-  ) => void;
+  onDragOver: (e: React.DragEvent<HTMLDivElement>, targetIndex: number) => void;
 };
 
 const Card = ({
@@ -31,12 +29,15 @@ const Card = ({
   onDragOver,
   targetIndex,
 }: CardTypeProps) => {
+  const editCard = useBoardStore((state) => state.editCard);
+  const cardLists = useBoardStore((state) => state.cardLists);
   const [isEditing, setIsEditing] = React.useState(false);
   const [newText, setNewText] = React.useState(dataCard.text);
-  const editCard = useBoardStore((state) => state.editCard);
   const colors = statusColors[listType];
   const [isDragging, setIsDragging] = useState(false);
+  const moveCard = useBoardStore((state) => state.moveCard);
 
+  // Handle-Status-Input
   function handleIsEditCard(
     e: React.FormEvent<HTMLDivElement | HTMLButtonElement>,
   ) {
@@ -44,6 +45,7 @@ const Card = ({
     setIsEditing(true);
   }
 
+  // ----Handle-Logic-EditCard------
   function handleEditCard(e: React.KeyboardEvent<HTMLInputElement>) {
     if (e.key === "Enter") {
       if (!newText.trim()) {
@@ -63,9 +65,29 @@ const Card = ({
     }
   }
 
+  function handleCardKeyDown(e: React.KeyboardEvent<HTMLDivElement>) {
+    if (e.key !== "ArrowRight" && e.key !== "ArrowLeft" ) {
+    return
+    }
+      e.preventDefault();
+
+      const currentListIndex = cardLists.findIndex(
+        (list) => list.id === sourceListId,
+      );
+      const direction = e.key === "ArrowRight" ? 1 : -1
+      const targetList = cardLists[currentListIndex + direction];
+
+      if (!targetList) {
+        return;
+      }
+      moveCard(dataCard.id, sourceListId, targetList?.id);
+
+  }
+
   return (
     <motion.div
       layout
+      tabIndex={0}
       initial={{ opacity: 0, y: 14, scale: 0.95 }}
       whileInView={{ opacity: 1, y: 0, scale: 1 }}
       animate={{
@@ -83,7 +105,7 @@ const Card = ({
       }}
       whileTap={{ scale: 0.97 }}
       whileDrag={{ scale: 0.97 }}
-      className={`bg-card text-card-foreground rounded-sm flex items-center justify-between border p-4 text-sm font-sans hover:cursor-grab group h-16 border-l-4 ${colors.border}`}
+      className={`bg-card text-card-foreground rounded-sm flex items-center justify-between border p-4 text-sm font-sans hover:cursor-grab group h-16 border-l-4 focus-visible:outline-0 focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 ${colors.border}`}
       onDragOver={(e) => onDragOver(e, targetIndex)}
       draggable={isEditing ? false : true}
       onDragStart={(e) => {
@@ -92,6 +114,7 @@ const Card = ({
       }}
       onDragEnd={() => setIsDragging(false)}
       onDoubleClick={handleIsEditCard}
+      onKeyDown={handleCardKeyDown}
     >
       {isEditing ? (
         <input
@@ -101,6 +124,7 @@ const Card = ({
             setNewText(e.target.value)
           }
           onBlur={() => {
+            setNewText(dataCard.text);
             setIsEditing(false);
           }}
           onKeyDown={handleEditCard}
@@ -117,10 +141,7 @@ const Card = ({
         }`}
       >
         <EditCardButton onClick={handleIsEditCard} />
-        <DeleteCardButton
-          listId={sourceListId}
-          cardId={dataCard.id}
-        />
+        <DeleteCardButton listId={sourceListId} cardId={dataCard.id} />
       </div>
     </motion.div>
   );
